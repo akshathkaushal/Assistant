3ac629476fe723b53345a8535a4cc4d5b28a12e5
akshathkaushal <akshath.kaushal@iiitb.org>
2020-04-04 18:31:13 +0530

    Started the project

---
+++ main/GUI/main.js
@@ -0,0 +1,39 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function launch() {
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    win.on('close', () => {
+        win = null;
+    });
+}
+
+app.on('ready', launch);
+
+app.on('window-all-closed', () => {
+    if(process.platform != 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if(win==null){
+        launch()
+    }
+});
