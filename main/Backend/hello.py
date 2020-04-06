a7943e3192f6d3376d12f8e141b85ce226575881
akshathkaushal <akshath.kaushal@iiitb.org>
2020-04-06 23:45:30 +0530

    Integrated Python3 and ElectronJS

--- main/Backend/hello.py
+++ main/Backend/hello.py
@@ -1,9 +1,9 @@
-import pyttsx3
+import pyttsx3, datetime, pyaudio, sys
+import speech_recognition as sr
+
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-rate = engine.getProperty('rate')
-
 engine.setProperty('voice', voices[14].id)
 engine.setProperty('rate', 140)
 
@@ -13,5 +13,40 @@ def speak(audio):
     engine.runAndWait()
 
 
-speak("Hello! This is python 3, talking to, Electron J S")
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 1
+        audio = r.listen(source, timeout=3, phrase_time_limit=3)
 
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio)
+
+    except sr.UnknownValueError:
+        print('Say that again please.')
+        return 'None'
+    return query
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour >= 0 and hour < 12:
+        speak('Good Morning sir.')
+    elif hour >= 12 and hour < 18:
+        speak('Good Afternoon sir.')
+    else:
+        speak('Good evening sir.')
+
+
+if __name__ == "__main__":
+    wishMe()
+    while 1:
+        query = takeCommand().lower()
+        speak(query)
+
+        if query == 'exit':
+            sys.exit()
